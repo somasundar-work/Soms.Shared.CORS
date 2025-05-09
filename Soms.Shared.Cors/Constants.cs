969ad27a5f74c257e45248ac7d1de95103e99eb1
Somasundar <somasundar.work@outlook.com>
2025-05-10 02:07:44 +0530

    feat: minor version trigger

--- Soms.Shared.Cors/Constants.cs
+++ Soms.Shared.Cors/Constants.cs
@@ -1,6 +1,12 @@
 namespace Soms.Shared.Cors;
 
+/// <summary>
+/// Contains constant values used for CORS configuration.
+/// </summary>
 internal static class Constants
 {
+    /// <summary>
+    /// The default name used for identifying the CORS policy.
+    /// </summary>
     public const string DefaultPolicyName = "defaultPolicy";
 }
